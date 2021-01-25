24ae8e50d409b7dcbb5b04c0eb1ed64f75f91e92
Ian Parberry <ian@unt.edu>
2021-01-25 15:23:54 -0600

    Fixing merge conflict.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
 int main(){
-<<<<<<< HEAD
-  std::cout << "Hello World from Windows!" << std::endl;
-=======
-  std::cout << "Hello World from cse5!" << std::endl;
->>>>>>> cba1f8ce93e7debf8c5d3ac81f04384b86bea087
+  std::cout << "Hello World from merged cse05 and Windows!" << std::endl;
   return 0;
 } //main
