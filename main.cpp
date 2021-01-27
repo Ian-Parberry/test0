ded0ead05bd879299cfa66d6c188be1e2cc052fc
Bob <ian@unt.edu>
2021-01-27 10:14:15 -0600

    Hello from Bob

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Hello World from merged cse05 and Windows!" << std::endl;
+  std::cout << "Hello World from Windows!" << std::endl;
   return 0;
 } //main
